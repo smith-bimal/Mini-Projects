5af359c8bb0695425712d2c6c3cadce4c487a6c4
{PIIOT23DEC2508} <{golokbihareebeemal@gmail.com}>
2024-02-06 16:26:16 +0530

    updated the code

--- Projects/9. Tic-Tac-Toe Game/app2.js
+++ Projects/9. Tic-Tac-Toe Game/app2.js
@@ -3,7 +3,13 @@ let newGame = document.querySelector("#new-game-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 let boxes = document.querySelectorAll(".box");
+let smallWin = document.querySelector("#small-msg");
 let turnO = false;
+let playerO = document.querySelector("#playerOScore");
+let countO = 0;
+let playerX = document.querySelector("#playerXScore");
+let countX = 0;
+
 let winningArr = [
     [0, 1, 2],
     [3, 4, 5],
@@ -20,9 +26,11 @@ boxes.forEach((box) => {
         if (turnO) {
             box.innerText = "X";
             turnO = false;
+            box.style.color = ("#8052ec");
         } else {
             box.innerText = "O";
             turnO = true;
+            box.style.color = ("#d161ff");
         }
         box.disabled = true;
         checkWinner();
@@ -35,12 +43,26 @@ let checkWinner = () => {
         let val2 = boxes[idx[1]].innerText;
         let val3 = boxes[idx[2]].innerText;
 
-        if (val1 != "" && val1 != "" && val1 != "") {
+        if (val1 != "" && val2 != "" && val3 != "") {
             if (val1 === val2 && val2 === val3) {
-                msg.innerText = `Player ${val1} is the winner.`
-                msgContainer.style.opacity = "1";
-                msgContainer.style.transform = "scale(1)";
                 disableBoxes();
+
+                if (val1 == "X") {
+                    countX++;
+                    playerX.innerText = countX;
+                    smallWin.innerText = "Player X won.";
+
+                } else {
+                    countO++;
+                    playerO.innerText = countO;
+                    smallWin.innerText = "Player O won.";
+                }
+
+                if (countX == 5 || countO == 5) {
+                    msg.innerText = `Player ${val1} is the final winner.`
+                    msgContainer.style.opacity = "1";
+                    msgContainer.style.transform = "scale(1)";
+                }
             }
         }
     }
@@ -56,14 +78,20 @@ resetBtn.addEventListener("click", () => {
     for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
+        smallWin.innerText = "";
     }
 })
 
 newGame.addEventListener("click", () => {
     for (let box of boxes) {
+        countX = 0;
+        countO = 0;
         msgContainer.style.opacity = "0";
         msgContainer.style.transform = "scale(0)";
         box.disabled = false;
+        smallWin.innerText = "";
         box.innerText = "";
+        playerO.innerText = countO;
+        playerX.innerText = countX;
     }
 })
